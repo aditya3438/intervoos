f53e53f2938dfc6890ee6c26ad26fe43bee0dc70
SaiChandra2896 <saichandra2896@gmail.com>
2019-05-08 17:28:54 +0530

    configured mongodb

---
+++ backend/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://sai:12345@intervoos-xqewk.mongodb.net/test?retryWrites=true",
+  secretOrkey: "intervoos"
+};
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-// write your server code here
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("server is up on port " + port);
+});
